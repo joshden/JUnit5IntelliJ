dc87e1152b99be28f2e8a0e41cac039aaa7d03a3
Joshua Hayden <joshden@gmail.com>
2017-01-17 13:53:56 -0600

    Add getAge() with a brittle test

--- src/main/java/com/example/Person.java
+++ src/main/java/com/example/Person.java
@@ -1,15 +1,30 @@
 package com.example;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 class Person {
     private final String givenName;
     private final String surname;
+    private final LocalDate dateOfBirth;
 
-    Person(String givenName, String surname) {
+    Person(String givenName, String surname, LocalDate dateOfBirth) {
         this.givenName = givenName;
         this.surname = surname;
+        this.dateOfBirth = dateOfBirth;
     }
 
     String getDisplayName() {
         return surname + ", " + givenName;
+    }
+
+    long getAge() {
+        return ChronoUnit.YEARS.between(dateOfBirth, LocalDate.now());
+    }
+
+    public static void main(String... args) {
+        Person person = new Person("Joey", "Doe", LocalDate.parse("2013-01-12"));
+        System.out.println(person.getDisplayName() + ": " + person.getAge() + " years");
+        // Doe, Joey: 4 years
     }
 }
--- src/test/java/com/example/PersonTest.java
+++ src/test/java/com/example/PersonTest.java
@@ -1,15 +1,25 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PersonTest {
 
     @Test
     void testGetDisplayName() {
-        Person person = new Person("Josh", "Hayden");
+        Person person = new Person("Josh", "Hayden", null);
         String displayName = person.getDisplayName();
         assertEquals("Hayden, Josh", displayName);
+    }
+
+    @Test
+    void testGetAge() {
+        Person person = new Person("Joey", "Doe", LocalDate.parse("2013-01-12"));
+        long age = person.getAge();
+        assertEquals(4, age);
     }
 
 }
