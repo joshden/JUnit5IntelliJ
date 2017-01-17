38aa168ef8d1312223ecf0b52952dec5ffd4905f
Joshua Hayden <joshden@gmail.com>
2017-01-17 17:18:36 -0600

    Add initial test for publishAge

--- src/test/java/com/example/PersonTest.java
+++ src/test/java/com/example/PersonTest.java
@@ -1,9 +1,7 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-
 import java.time.LocalDate;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PersonTest {
@@ -22,6 +20,14 @@ class PersonTest {
         Person person = new Person("Joey", "Doe", dateOfBirth, ()->currentDate);
         long age = person.getAge();
         assertEquals(4, age);
+    }
+
+    @Test
+    void testPublishAge() {
+        LocalDate dateOfBirth = LocalDate.parse("2000-01-02");
+        LocalDate currentDate = LocalDate.parse("2017-01-01");
+        Person person = new Person("Joe", "Sixteen", dateOfBirth, ()->currentDate);
+        person.publishAge();
     }
 
 }
